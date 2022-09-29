1057d56a756be740dc53cd539cf1794a5fc4382f
Nahuel Caroseli <nahucaroseli2100@gmail.com>
2022-09-29 19:00:42 -0300

    nos confundimos de repo asi q pasamos todo aca

---
+++ tp2/js/script.js
@@ -0,0 +1,30 @@
+"user strict"
+document.addEventListener("DOMContentLoaded", () => {
+
+    loader();
+
+    function loader() {
+        setTimeout(function () {
+            document.getElementById('spinner').style.visibility = "hidden";
+            document.getElementById('content').style.visibility = "visible";
+        }, 5000);
+    }
+
+
+
+    const burger = document.querySelector(".menu");
+    const close = document.querySelector(".close-icon");
+    const userMenu = document.querySelector(".menu-user");
+    
+    close.addEventListener("click", () =>{
+        userMenu.classList.remove('active');
+    })
+   
+    burger.addEventListener("click",() =>{
+        userMenu.classList.toggle('active');
+    })
+
+    
+
+})
+
