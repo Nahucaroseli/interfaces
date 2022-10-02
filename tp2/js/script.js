91f9c3fa79d009f572c1e4824a661f7d11980f11
Nahuel Caroseli <nahucaroseli2100@gmail.com>
2022-10-02 10:48:27 -0300

    agregado carousel personajes y corregido hover de cards

--- tp2/js/script.js
+++ tp2/js/script.js
@@ -26,5 +26,43 @@ document.addEventListener("DOMContentLoaded", () => {
 
     
 
+    const slider = document.querySelector(".slider-inner");
+    const progressbar = document.querySelector(".progress-bar-inner");
+
+    let slidergrabbed = false;
+
+    slider.parentElement.addEventListener("scroll", (e) =>{
+        progressbar.style.width = `${getScrollPercentage()}%`;
+    })
+
+    slider.addEventListener("mousedown", (e) =>{
+        slidergrabbed = true;
+        slider.style.cursor = "grabbing";
+
+    })
+    slider.addEventListener("mouseup", (e) =>{
+        slidergrabbed = false;
+        slider.style.cursor = "grab";
+        
+    })
+
+    slider.addEventListener("mouseleave", (e) =>{
+        slidergrabbed = false;
+        slider.style.cursor = "grab";
+        
+    })
+    
+    slider.addEventListener("mousemove", (e) =>{
+        if(slidergrabbed){
+            slider.parentElement.scrollLeft -= e.movementX;
+        }
+        
+    })
+
+    function getScrollPercentage(){
+        return ((slider.parentElement.scrollLeft/(slider.parentElement.scrollWidth - slider.parentElement.clientWidth)) *100);
+    }
+
+
 })
 
