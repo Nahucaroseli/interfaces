a8d36ea551a12396ee7f02b1781b4fb727ff9dca
Nahuel Caroseli <nahucaroseli2100@gmail.com>
2022-10-05 19:10:06 -0300

    Agregado menu hamburguesa con categorias y menu perfil

--- tp2/js/script.js
+++ tp2/js/script.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     loader();
-
+    //Loader
     function loader() {
         setTimeout(function () {
             document.getElementById('spinner').style.visibility = "hidden";
@@ -11,21 +11,70 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
 
-
-    const burger = document.querySelector(".menu");
-    const close = document.querySelector(".close-icon");
+    //burger menu
+    const profile = document.querySelector(".profile");
+    const menuIcon = document.querySelector(".menu");
+    const burgerMenu = document.querySelector(".burger-menu");
+    const close = document.querySelectorAll(".close-icon");
     const userMenu = document.querySelector(".menu-user");
     
-    close.addEventListener("click", () =>{
-        userMenu.classList.remove('active');
+    close.forEach(element=>{
+        element.addEventListener("click",()=>{
+                burgerMenu.classList.remove("active");
+                userMenu.classList.remove("active");
+        })
     })
    
-    burger.addEventListener("click",() =>{
+    profile.addEventListener("click",() =>{
         userMenu.classList.toggle('active');
+
+    })
+    menuIcon.addEventListener("click",() =>{
+        burgerMenu.classList.toggle('active');
+
     })
 
-    
 
+    //Carousel Main Index
+    const buttons = document.querySelectorAll("[data-carousel-button]");
+
+    buttons.forEach(button =>{
+        button.addEventListener("click", ()=>{
+            const offset = button.dataset.carouselButton === "next" ? 1 : -1
+            const offsetText = button.dataset.carouselText === "next" ? 1 : -1
+            const slides = button.closest("[data-carousel").querySelector("[data-slides]");
+            const texts = button.closest("[data-carousel").querySelector("[data-text]");
+
+
+            const activeSlide = slides.querySelector("[data-active]");
+            const activeText = texts.querySelector("[data-text-active]");
+
+
+            let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+            console.log(newIndex);
+            let newIndexText = [...texts.children].indexOf(activeText) + offsetText;
+
+            if(newIndex < 0 && newIndexText <0){
+                newIndex = slides.children.length -1;
+                newIndexText = texts.children.length -1;
+             
+            }
+            if(newIndex >=slides.children.length && newIndexText >=texts.children.length)
+            {
+                newIndex = 0;
+                newIndexText = 0;
+        }
+            slides.children[newIndex].dataset.active = true
+            delete activeSlide.dataset.active
+            texts.children[newIndexText].dataset.textActive = true
+            delete activeText.dataset.textActive
+        })
+    })
+
+
+
+    
+    //carousel with progress bar
     const slider = document.querySelectorAll(".slider-inner");
     const progressbar = document.querySelectorAll(".progress-bar-inner");
 
