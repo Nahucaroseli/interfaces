6eefa31d3357ccb73fc9a2789aa8a036b4201b1a
NahuCaroseli <nahucaroseli2100@gmail.com>
2022-11-24 16:13:19 -0300

    Agregado titulo que se acerca a las cards de personajes en scroll

--- tp4/js/comingGame.js
+++ tp4/js/comingGame.js
@@ -18,12 +18,15 @@ document.querySelector(".story-section").addEventListener('scroll',()=>{
 
 let character = document.querySelector(".hero-section-mask");
 let gameTitle = document.querySelector(".hero-section-game-title");
+let titleCharacterSection = document.getElementById("title-characters-section");
 
 window.addEventListener('scroll',()=>{
         const scrolled = window.pageYOffset;//guardamos el valor del scroll total que realizo el usuario
         const val = scrolled*0.5;
         character.style.transform = `translateX(-${0.30*val}%)`;//agregamos una transformacion al personaje 
         gameTitle.style.transform = `translateX(${0.30*val}%)`;//agregamos una transformacion al titulo del juego
+        titleCharacterSection.style.transform = `translateY(${0.28*val}px)`;
+        titleCharacterSection.style.opacity = 1;
 
     
 })
